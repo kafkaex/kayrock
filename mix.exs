6e01519a016b4c1600f9565a4cbb45a9569f305f
Dan Swain <dan.t.swain@gmail.com>
2019-05-26 18:24:33 -0400

    CI tweaks, update README

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Kayrock.MixProject do
     [
       app: :kayrock,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
