e556bf2ed769b37fbf71ea0084c97fa48dea0d77
Dan Swain <dan.t.swain@gmail.com>
2019-05-27 19:54:34 -0400

    Working docker set up with Kafka 2.2.0

--- scripts/docker_up.sh
+++ scripts/docker_up.sh
@@ -25,26 +25,6 @@ export DOCKER_IP=$(ifconfig ${IP_IFACE} | grep 'inet ' | awk '{print $2}' | cut 
 # for debugging purposes
 echo Detected active network interface ${IP_IFACE} with ip ${DOCKER_IP}
 
-for i in 1 2 3
-do
-  port=$(expr 9092 + ${i} - 1)
-  mkdir -p kafka${i}
-  target=kafka${i}/server.properties.in
-  cp ./server.properties ${target}
-  # configure broker and port
-  sed -i.bak "s/@broker_id@/${i}/g" ${target}
-  sed -i.bak "s/@port@/${port}/g" ${target}
-  # the @pwd become root directory so we get /ssl
-  sed -i.bak "s|@pwd@||g" ${target}
-  # point at zookeeper in docker
-  sed -i.bak "s/localhost:2181/zookeeper:2181/" ${target}
-  # delete the existing listeners line
-  sed -i.bak "/^listeners=/d" ${target}
-  # add an advertised.listeners and listeners together at the end
-  echo "advertised.listeners=SSL://${DOCKER_IP}:${port}" >> ${target}
-  echo "listeners=SSL://0.0.0.0:${port}" >> ${target}
-done
-
 docker-compose up -d
 
 # create topics needed for testing
