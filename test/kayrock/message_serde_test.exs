158d68e7faa4bb7fdf017778dc89ca3eb533bacd
Dan Swain <dan.t.swain@gmail.com>
2019-06-09 20:45:23 -0400

    Add compression serde tests

--- test/kayrock/message_serde_test.exs
+++ test/kayrock/message_serde_test.exs
@@ -135,4 +135,213 @@ defmodule Kayrock.MessageSerdeTest do
 
     assert got == expect, compare_binaries(got, expect)
   end
+
+  test "message format 2 serialization with snappy compression" do
+    record_batch = %Kayrock.RecordBatch{
+      attributes: 2,
+      base_sequence: -1,
+      batch_length: nil,
+      batch_offset: 0,
+      crc: nil,
+      first_timestamp: -1,
+      last_offset_delta: -1,
+      max_timestamp: -1,
+      partition_leader_epoch: -1,
+      producer_epoch: -1,
+      producer_id: -1,
+      records: [
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 0,
+          timestamp: -1,
+          value: "foo"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 0,
+          timestamp: -1,
+          value: "bar"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 0,
+          timestamp: -1,
+          value: "baz"
+        }
+      ]
+    }
+
+    expect =
+      <<0, 0, 0, 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 78, 255, 255, 255, 255, 2, 240, 195, 168,
+        31, 0, 2, 0, 0, 0, 2, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0,
+        0, 3, 30, 36, 18, 0, 0, 0, 1, 6, 102, 111, 111, 0, 9, 10, 52, 98, 97, 114, 0, 18, 0, 0, 0,
+        1, 6, 98, 97, 122, 0>>
+
+    got = IO.iodata_to_binary(RecordBatch.serialize(record_batch))
+    assert got == expect, compare_binaries(got, expect)
+  end
+
+  test "message format 2 deserialization with snappy compression" do
+    data =
+      <<0, 0, 0, 0, 0, 0, 0, 126, 0, 0, 0, 101, 0, 0, 0, 4, 2, 27, 231, 230, 245, 0, 2, 0, 0, 0,
+        2, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 3, 130, 83, 78,
+        65, 80, 80, 89, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 32, 30, 116, 18, 0, 0, 0, 1, 6, 102,
+        111, 111, 0, 18, 0, 0, 2, 1, 6, 98, 97, 114, 0, 18, 0, 0, 4, 1, 6, 98, 97, 122, 0>>
+
+    expect = %Kayrock.RecordBatch{
+      attributes: 2,
+      base_sequence: -1,
+      batch_length: 101,
+      batch_offset: 126,
+      crc: 468_182_773,
+      first_timestamp: -1,
+      last_offset_delta: 2,
+      max_timestamp: -1,
+      partition_leader_epoch: 4,
+      producer_epoch: -1,
+      producer_id: -1,
+      records: [
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 126,
+          timestamp: -1,
+          value: "foo"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 127,
+          timestamp: -1,
+          value: "bar"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 128,
+          timestamp: -1,
+          value: "baz"
+        }
+      ]
+    }
+
+    got = RecordBatch.deserialize(data)
+    assert got == expect
+  end
+
+  test "serialize v2 message with gzip compression" do
+    record_batch = %Kayrock.RecordBatch{
+      attributes: 1,
+      base_sequence: -1,
+      batch_length: nil,
+      batch_offset: 0,
+      crc: nil,
+      first_timestamp: -1,
+      last_offset_delta: -1,
+      max_timestamp: -1,
+      partition_leader_epoch: -1,
+      producer_epoch: -1,
+      producer_id: -1,
+      records: [
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 0,
+          timestamp: -1,
+          value: "foo"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 0,
+          timestamp: -1,
+          value: "bar"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 0,
+          timestamp: -1,
+          value: "baz"
+        }
+      ]
+    }
+
+    expect =
+      <<0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 88, 255, 255, 255, 255, 2, 114, 69, 255,
+        84, 0, 1, 0, 0, 0, 2, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0,
+        0, 3, 31, 139, 8, 0, 0, 0, 0, 0, 0, 19, 19, 98, 96, 96, 96, 100, 75, 203, 207, 103, 16, 2,
+        179, 146, 18, 139, 224, 172, 42, 6, 0, 160, 136, 44, 185, 30, 0, 0, 0>>
+
+    got = IO.iodata_to_binary(RecordBatch.serialize(record_batch))
+
+    assert got == expect, compare_binaries(got, expect)
+  end
+
+  test "deserialize v2 message with gzip compression" do
+    data =
+      <<0, 0, 0, 0, 0, 0, 0, 132, 0, 0, 0, 94, 0, 0, 0, 4, 2, 108, 148, 172, 111, 0, 1, 0, 0, 0,
+        2, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+        255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 3, 31, 139, 8,
+        0, 0, 0, 0, 0, 0, 0, 19, 98, 96, 96, 96, 100, 75, 203, 207, 103, 16, 98, 96, 96, 98, 100,
+        75, 74, 44, 2, 177, 88, 64, 172, 42, 6, 0, 116, 60, 95, 153, 30, 0, 0, 0>>
+
+    expect = %Kayrock.RecordBatch{
+      attributes: 1,
+      base_sequence: -1,
+      batch_length: 94,
+      batch_offset: 132,
+      crc: 1_821_682_799,
+      first_timestamp: -1,
+      last_offset_delta: 2,
+      max_timestamp: -1,
+      partition_leader_epoch: 4,
+      producer_epoch: -1,
+      producer_id: -1,
+      records: [
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 132,
+          timestamp: -1,
+          value: "foo"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 133,
+          timestamp: -1,
+          value: "bar"
+        },
+        %Kayrock.RecordBatch.Record{
+          attributes: 0,
+          headers: <<0>>,
+          key: nil,
+          offset: 134,
+          timestamp: -1,
+          value: "baz"
+        }
+      ]
+    }
+
+    got = RecordBatch.deserialize(data)
+    assert got == expect
+  end
 end
