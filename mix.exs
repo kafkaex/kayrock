6126d40fb522da45f1914811ddabb86c7920686c
Dan Swain <dan.t.swain@gmail.com>
2019-10-11 18:40:26 -0400

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kayrock.MixProject do
   def project do
     [
       app: :kayrock,
-      version: "0.1.8",
+      version: "0.1.9",
       elixir: "~> 1.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
