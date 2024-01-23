333056a599c6d3c3b8c2b807a6787a9dfa4d6f21
Argonus <argonuspiotr@gmail.com>
2024-01-23 12:04:12 +0100

    Add compression & producer tests

--- test/integration/producer_test.exs
+++ test/integration/producer_test.exs
@@ -50,6 +50,71 @@ defmodule Kayrock.Integration.ProducerTest do
         # [THEN] Verify message data
         [message] = List.first(response.partition_responses).record_set.messages
         assert message.value == "test"
+        assert message.offset == offset
+      end
+
+      test "v#{version} - produce and reads data using message set with multiple messages", %{
+        kafka: kafka
+      } do
+        api_version = unquote(version)
+        {:ok, client_pid} = build_client(kafka)
+
+        # Create Topic
+        topic_name = create_topic(client_pid, api_version)
+
+        # [GIVEN] MessageSet with timestamp
+        record_set = %Kayrock.MessageSet{
+          messages: [
+            %Kayrock.MessageSet.Message{
+              key: "1",
+              value: "foo",
+              attributes: 0
+            },
+            %Kayrock.MessageSet.Message{
+              key: "1",
+              value: "bar",
+              attributes: 0
+            },
+            %Kayrock.MessageSet.Message{
+              key: "1",
+              value: "baz",
+              attributes: 0
+            }
+          ]
+        }
+
+        # [WHEN] Produce message with timestamp
+        produce_message_request =
+          produce_messages_request(topic_name, [[record_set: record_set]], 1, api_version)
+
+        {:ok, resp} = Kayrock.client_call(client_pid, produce_message_request, :controller)
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        [partition_response] = response.partition_responses
+        assert partition_response.error_code == 0
+        offset = partition_response.base_offset
+
+        # [THEN] Fetch message from topic
+        partition_data = [[topic: topic_name, partition: 0, fetch_offset: offset]]
+        fetch_request = fetch_messages_request(partition_data, [], api_version)
+        {:ok, resp} = Kayrock.client_call(client_pid, fetch_request, :controller)
+
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        # [THEN] Verify message data
+        [message_one, message_two, message_three] =
+          List.first(response.partition_responses).record_set.messages
+
+        assert message_one.value == "foo"
+        assert message_one.offset == offset
+
+        assert message_two.value == "bar"
+        assert message_two.offset == offset + 1
+
+        assert message_three.value == "baz"
+        assert message_three.offset == offset + 2
       end
     end
 
@@ -99,6 +164,112 @@ defmodule Kayrock.Integration.ProducerTest do
         # [THEN] Verify message data
         [message] = List.first(response.partition_responses).record_set.messages
         assert message.value == "test"
+        assert message.timestamp == timestamp
+      end
+
+      test "v#{version} - produce and reads data using message set with multiple messages", %{
+        kafka: kafka
+      } do
+        api_version = unquote(version)
+        {:ok, client_pid} = build_client(kafka)
+
+        # Create Topic
+        topic_name = create_topic(client_pid, api_version)
+        timestamp = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
+
+        # [GIVEN] MessageSet with timestamp
+        records = [
+          %Kayrock.RecordBatch.Record{
+            key: "1",
+            value: "foo",
+            timestamp: timestamp,
+            attributes: 0
+          },
+          %Kayrock.RecordBatch.Record{
+            key: "1",
+            value: "bar",
+            timestamp: timestamp,
+            attributes: 0
+          },
+          %Kayrock.RecordBatch.Record{
+            key: "1",
+            value: "baz",
+            timestamp: timestamp,
+            attributes: 0
+          }
+        ]
+
+        # [WHEN] Produce message with timestamp
+        record_set = %Kayrock.RecordBatch{records: records}
+
+        produce_message_request =
+          produce_messages_request(topic_name, [[record_set: record_set]], 1, api_version)
+
+        {:ok, resp} = Kayrock.client_call(client_pid, produce_message_request, :controller)
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        [partition_response] = response.partition_responses
+        assert partition_response.error_code == 0
+
+        # [THEN] Fetch message from topic
+        partition_data = [[topic: topic_name, partition: 0, fetch_offset: 0, log_start_offset: 0]]
+        fetch_request = fetch_messages_request(partition_data, [], api_version)
+        {:ok, resp} = Kayrock.client_call(client_pid, fetch_request, :controller)
+
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        # [THEN] Verify message data
+        [message_one, message_two, message_three] =
+          List.first(response.partition_responses).record_set.messages
+
+        assert message_one.value == "foo"
+        assert message_one.offset == 0
+        assert message_one.timestamp == timestamp
+
+        assert message_two.value == "bar"
+        assert message_two.offset == 1
+        assert message_two.timestamp == timestamp
+
+        assert message_three.value == "baz"
+        assert message_three.offset == 2
+        assert message_three.timestamp == timestamp
+
+        # [THEN] Produce another message
+        record = %Kayrock.RecordBatch.Record{
+          key: "1",
+          value: "zab",
+          timestamp: timestamp,
+          attributes: 0
+        }
+
+        record_set = %Kayrock.RecordBatch{records: [record]}
+
+        # [WHEN] Produce message with timestamp
+        produce_message_request =
+          produce_messages_request(topic_name, [[record_set: record_set]], 1, api_version)
+
+        {:ok, resp} = Kayrock.client_call(client_pid, produce_message_request, :controller)
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        [partition_response] = response.partition_responses
+        assert partition_response.error_code == 0
+        offset = partition_response.base_offset
+
+        # [THEN] Fetch message from topic
+        partition_data = [[topic: topic_name, partition: 0, fetch_offset: offset]]
+        fetch_request = fetch_messages_request(partition_data, [], api_version)
+        {:ok, resp} = Kayrock.client_call(client_pid, fetch_request, :controller)
+
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        # [THEN] Verify message data
+        [message] = List.first(response.partition_responses).record_set.messages
+        assert message.value == "zab"
+        assert message.offset == 3
         assert message.timestamp == timestamp
       end
     end
@@ -153,6 +324,121 @@ defmodule Kayrock.Integration.ProducerTest do
         assert message.value == "test"
         assert message.timestamp == timestamp
         assert message.headers == [%Kayrock.RecordBatch.RecordHeader{key: "1", value: "1"}]
+      end
+
+      test "v#{version} - produce and reads data using message set with multiple messages", %{
+        kafka: kafka
+      } do
+        api_version = unquote(version)
+        {:ok, client_pid} = build_client(kafka)
+
+        # Create Topic
+        topic_name = create_topic(client_pid, api_version)
+        timestamp = DateTime.utc_now() |> DateTime.to_unix(:millisecond)
+
+        # [GIVEN] MessageSet with timestamp
+        record_set = %Kayrock.RecordBatch{
+          records: [
+            %Kayrock.RecordBatch.Record{
+              key: "1",
+              value: "foo",
+              timestamp: timestamp,
+              headers: [%Kayrock.RecordBatch.RecordHeader{key: "1", value: "1"}],
+              attributes: 0
+            },
+            %Kayrock.RecordBatch.Record{
+              key: "1",
+              value: "bar",
+              timestamp: timestamp,
+              headers: [%Kayrock.RecordBatch.RecordHeader{key: "1", value: "1"}],
+              attributes: 0
+            },
+            %Kayrock.RecordBatch.Record{
+              key: "1",
+              value: "baz",
+              timestamp: timestamp,
+              headers: [%Kayrock.RecordBatch.RecordHeader{key: "1", value: "1"}],
+              attributes: 0
+            }
+          ]
+        }
+
+        # [WHEN] Produce message with timestamp
+        produce_message_request =
+          produce_messages_request(topic_name, [[record_set: record_set]], 1, api_version)
+
+        {:ok, resp} = Kayrock.client_call(client_pid, produce_message_request, :controller)
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        [partition_response] = response.partition_responses
+        assert partition_response.error_code == 0
+        offset = partition_response.base_offset
+
+        # [THEN] Fetch message from topic
+        partition_data = [[topic: topic_name, partition: 0, fetch_offset: offset]]
+        fetch_request = fetch_messages_request(partition_data, [], api_version)
+        {:ok, resp} = Kayrock.client_call(client_pid, fetch_request, :controller)
+
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        # [THEN] Verify message data
+        [message_one, message_two, message_three] =
+          List.first(response.partition_responses).record_set |> List.first() |> Map.get(:records)
+
+        assert message_one.value == "foo"
+        assert message_one.offset == 0
+        assert message_one.timestamp == timestamp
+        assert message_one.headers == [%Kayrock.RecordBatch.RecordHeader{key: "1", value: "1"}]
+
+        assert message_two.value == "bar"
+        assert message_two.offset == 1
+        assert message_two.timestamp == timestamp
+        assert message_two.headers == [%Kayrock.RecordBatch.RecordHeader{key: "1", value: "1"}]
+
+        assert message_three.value == "baz"
+        assert message_three.offset == 2
+        assert message_three.timestamp == timestamp
+        assert message_three.headers == [%Kayrock.RecordBatch.RecordHeader{key: "1", value: "1"}]
+
+        # [THEN] Produce another message
+        record = %Kayrock.RecordBatch.Record{
+          key: "1",
+          value: "zab",
+          timestamp: timestamp,
+          attributes: 0
+        }
+
+        record_set = %Kayrock.RecordBatch{records: [record]}
+
+        # [WHEN] Produce message with timestamp
+        produce_message_request =
+          produce_messages_request(topic_name, [[record_set: record_set]], 1, api_version)
+
+        {:ok, resp} = Kayrock.client_call(client_pid, produce_message_request, :controller)
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        [partition_response] = response.partition_responses
+        assert partition_response.error_code == 0
+        offset = partition_response.base_offset
+
+        # [THEN] Fetch message from topic
+        partition_data = [[topic: topic_name, partition: 0, fetch_offset: offset]]
+        fetch_request = fetch_messages_request(partition_data, [], api_version)
+        {:ok, resp} = Kayrock.client_call(client_pid, fetch_request, :controller)
+
+        [response] = resp.responses
+        assert response.topic == topic_name
+
+        # [THEN] Verify message data
+        [message] =
+          List.first(response.partition_responses).record_set |> List.first() |> Map.get(:records)
+
+        assert message.value == "zab"
+        assert message.offset == 3
+        assert message.timestamp == timestamp
       end
     end
   end
