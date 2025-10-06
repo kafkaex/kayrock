cb176a91b30e4274520dbd961c66044243e2279a
Anton Borisov <anton.borisov@fresha.com>
2025-10-06 14:56:10 +0100

    Remove zstd for now as we can't move cleanly to next version of kafka_protocol

--- lib/kayrock/compression.ex
+++ lib/kayrock/compression.ex
@@ -16,54 +16,43 @@ defmodule Kayrock.Compression do
   ## Compression Levels
   
   - **Gzip**: 1 (fastest) to 9 (best compression), default: 6
-  - **Zstandard**: 1 to 22, default: 3 (Kafka default)
   - **Snappy/LZ4**: No levels supported
   
   ## Dependencies
   
   - **Snappy**: `{:snappy, "~> 1.1"}` or `{:snappyer, "~> 1.2"}`
   - **LZ4**: `{:lz4b, "~> 0.2.0"}`
-  - **Zstandard**: OTP 27+ built-in or `{:ezstd, "~> 1.0"}`
 
   """
 
-  alias Kayrock.Compression.{Gzip, Snappy, Lz4, Zstd}
+  alias Kayrock.Compression.{Gzip, Snappy, Lz4}
 
-  @type codec_t :: :gzip | :snappy | :lz4 | :zstd
+  @type codec_t :: :gzip | :snappy | :lz4
 
   @codecs [
     gzip:   Gzip,
     snappy: Snappy,
-    lz4:    Lz4,
-    zstd:   Zstd
+    lz4:    Lz4
   ]
 
   @gzip_attr   Gzip.attr()
   @snappy_attr Snappy.attr()
   @lz4_attr    Lz4.attr()
-  @zstd_attr   Zstd.attr()
 
   # ---------- Decompress ----------
   def decompress(@gzip_attr, data),   do: Gzip.decompress(data)
   def decompress(@snappy_attr, data), do: Snappy.decompress(data)
   def decompress(@lz4_attr, data),    do: Lz4.decompress(data)
-  def decompress(@zstd_attr, data),   do: Zstd.decompress(data)
 
   # ---------- Compress simple ----------
   def compress(:gzip, data),   do: {Gzip.compress(data), Gzip.attr()}
   def compress(:snappy, data), do: {Snappy.compress(data), Snappy.attr()}
   def compress(:lz4, data),    do: {Lz4.compress(data), Lz4.attr()}
-  def compress(:zstd, data),   do: {Zstd.compress(data), Zstd.attr()}
 
   # ---------- Compress with opts ----------
   def compress(:gzip, data, opts) do
     level = Keyword.get(opts, :level)
     {Gzip.compress(data, level), Gzip.attr()}
-  end
-
-  def compress(:zstd, data, opts) do
-    level = Keyword.get(opts, :level)
-    {Zstd.compress(data, level), Zstd.attr()}
   end
 
   def compress(:snappy, data, _opts), do: compress(:snappy, data)
--- mix.exs
+++ mix.exs
@@ -55,7 +55,6 @@ defmodule Kayrock.MixProject do
       {:snappy, git: "https://github.com/fdmanana/snappy-erlang-nif", only: [:dev, :test]},
       {:snappyer, "~> 1.2", only: [:dev, :test]},
       {:lz4b, "~> 0.0.13", only: [:dev, :test]},
-      {:ezstd, "~> 1.0", only: [:dev, :test]},
       {:testcontainers, "1.9.0", only: [:dev, :test]}
     ]
   end
